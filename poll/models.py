dd68dc75497db67fc93815a9378eeb3f86e6c107
Ryan Ferris <r.ferris137@gmail.com>
2024-03-18 22:49:19 +0000

    initial model and template for poll app

--- poll/models.py
+++ poll/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Poll(models.Model):
+    title = models.CharField(max_length=250)
+    thumbs_up_count = models.IntegerField(default=0)
+    thumbs_down_count = models.IntegerField(default=0)
+
+class Vote(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    thumbs_up = models.BooleanField(default=True)
+    thumbs_down = models.BooleanField(default=False)
+
