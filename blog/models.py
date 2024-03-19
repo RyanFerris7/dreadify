43d2a6349217685d72f9bf5516c241e0c7e9680a
Ryan Ferris <r.ferris137@gmail.com>
2024-03-19 12:20:36 +0000

    correct additional typo on index

--- blog/models.py
+++ blog/models.py
@@ -7,11 +7,14 @@ import os
 
 
 def Default_Image():
+    """Provides default image for articles"""
     return os.path.join(settings.MEDIA_URL, 'article_images/defaultimage.jpg')
 
 class Post(models.Model):
+    """Model for articles"""
 
     class NewManager(models.Manager):
+        """Retrieves published posts using filter"""
         def get_queryset(self):
             return super().get_queryset().filter(status='publish')
 
@@ -40,15 +43,19 @@ class Post(models.Model):
     new_manager = NewManager()
 
     def get_absolute_url(self):
+        """Uses slug to define page url"""
         return reverse('blog:post_page', args=[self.slug])
     
     class Meta:
+        """Reverses ordering, puts new articles first"""
         ordering = ('-publish',)
 
     def __str__(self):
+        """For admin panel, returns legible string to make management easier"""
         return self.title
 
 class Comments(models.Model):
+    """Model for comments"""
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments', default=None)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField(max_length=250)
@@ -56,20 +63,32 @@ class Comments(models.Model):
     updated = models.DateTimeField()
 
     class Meta:
+        """Reverses comment ordering, also corrects issue with auto plurilisation of apps"""
         ordering = ('-created', '-updated')
         verbose_name_plural = "Comments"
 
     def __str__(self):
+        """For admin panel, returns legible string to make management easier"""
         return self.content[:25]
 
 
-
 class Poll(models.Model):
+    """
+    Model for polls.
+
+    Stores the poll title, and vote count.
+        
+    """
     title = models.CharField(max_length=250)
     thumbs_up_count = models.IntegerField(default=0)
     thumbs_down_count = models.IntegerField(default=0)
 
 class Vote(models.Model):
+    """
+    Model for voting.
+    
+    Stores the users login, retrieves the poll and gives options to cast vote.
+    """
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
     thumbs_up = models.BooleanField(default=True)
